3b8283c20bf8d159e2c2932ee608c04887217332
Skyler Sprecker <saspreck@gmail.com>
2019-03-04 15:02:23 -0600

    Eigth commit, started MesoInherit constructor

--- src/MesoInherit.java
+++ src/MesoInherit.java
@@ -1,5 +1,9 @@
 public class MesoInherit extends MesoAbstract {
 	
+	public MesoInherit(MesoStation StID) {
+		
+	}
+	
 	public int[] calAverage() {
 		int[] ASCIIvalues = new int[4];
 		for(int i = 0; i < 4; i++) {
--- src/MesoStation.java
+++ src/MesoStation.java
@@ -1,4 +1,3 @@
-
 public class MesoStation 
 {
 	private String StID;
