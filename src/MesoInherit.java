73241ebca4357f6c962109020a30cf62edb9967d
Skyler Sprecker <saspreck@gmail.com>
2019-03-04 19:08:40 -0600

    Twenty fifth commit, added comments

--- src/MesoInherit.java
+++ src/MesoInherit.java
@@ -23,13 +23,18 @@ public class MesoInherit extends MesoAbstract {
 		double average = total / 4.0;
 		ASCIIvalues[2] = (int) average;
 		
+		//puts the floor and ceiling values into the array
 		ASCIIvalues[0] = (int) Math.ceil(average);
 		ASCIIvalues[1] = (int) Math.floor(average);
 		
+		// returns the array
 		return ASCIIvalues;
 	}
 	
+	//implementation of letterAverage method inherited from MesoAbstract class
 	public char letterAverage() {
+		
+		//returns the ASCII character equals to the average value
 		return (char) ASCIIvalues[2];
 	}
 }
